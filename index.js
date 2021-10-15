80e277fa16c66f1948650b290eb3eaa4f91dca0e
Kindred Lee <kindred4lee@gmail.com>
2021-10-14 23:04:39 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun (activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (doSomething = "go to the office") {
+    return `This Monday, I will ${doSomething}.`;
+}
+
+  function wrapAdjective (adjective="*") {
+      return function inner(something="special") {
+          return `You are ${adjective}${something}${adjective}!`
+      }
+  }
